990c7004207e6c9e026a6f9896ef9168b24e27f7
shivanggarg1998 <shivanggarg1998@gmail.com>
2020-11-18 08:53:27 +0530

    graphs updated, login page made

--- src/views/dashboard/Dashboard.js
+++ src/views/dashboard/Dashboard.js
@@ -6,12 +6,19 @@ import {
     CCol,
     CRow,
     CFormGroup,
-    CSelect
+    CSelect,
+    CProgress,
+    CCallout,
+    CModal,
+    CModalBody,
+    CModalHeader,
+    CBadge
 } from '@coreui/react'
 import {
     CChartBar,
     CChartPie
 } from '@coreui/react-chartjs'
+import CIcon from '@coreui/icons-react'
 
 class Dashboard extends Component {
     constructor(props) {
@@ -24,6 +31,7 @@ class Dashboard extends Component {
             country: 'India',
             date: Date.now(),
             loading: false,
+            modal: false,
         }
     }
     showGraph() {
@@ -54,7 +62,7 @@ class Dashboard extends Component {
                                     </CCol>
 
 
-                                    <CCol sm="4" className="d-none d-md-block">
+                                    <CCol sm="6" className="d-none d-md-block">
                                         <CRow>
                                             <CCol>
                                                 <CFormGroup>
@@ -81,13 +89,18 @@ class Dashboard extends Component {
                                             <CCol>
                                                 {this.state.loading == false ?
                                                     (<CButton class="btn btn-primary" onClick={() => { this.showGraph() }}>
-                                                        <span role="status" aria-hidden="true"> Display Graph </span>
+                                                        <span role="status" aria-hidden="true"> Show Prediction </span>
                                                     </CButton>)
                                                     : (<CButton class="btn btn-primary" type="button" >
                                                         <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
                                                         <span> &nbsp; Loading </span>
                                                     </CButton>)
                                                 }
+                                            </CCol>
+                                            <CCol>
+                                                <CButton class="btn btn-primary float-right" onClick={() => { this.setState({ modal: true }) }}>
+                                                    <span role="status" aria-hidden="true"> Show Analytics </span>
+                                                </CButton>
                                             </CCol>
                                         </CRow>
                                     </CCol>
@@ -405,6 +418,250 @@ class Dashboard extends Component {
                             </> : ''
                     }
                 </>
+                <CModal show={this.state.modal}
+                    onClose={() => { this.setState({ modal: false }) }}
+                    size="lg">
+                    <CModalHeader>
+                        Clothes predictive Analysis
+                    </CModalHeader>
+                    <CModalBody>
+                        <CRow>
+                            <CCol xs="12" md="6" xl="6">
+
+                                <CRow>
+                                    <CCol sm="6">
+                                        <CCallout color="info">
+                                            <small className="text-muted">New Sales</small>
+                                            <br />
+                                            <strong className="h4">9,123</strong>
+                                        </CCallout>
+                                    </CCol>
+                                    <CCol sm="6">
+                                        <CCallout color="danger">
+                                            <small className="text-muted">Recurring Sales</small>
+                                            <br />
+                                            <strong className="h4">22,643</strong>
+                                        </CCallout>
+                                    </CCol>
+                                </CRow>
+
+                                <hr className="mt-0" />
+
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            January
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="34" />
+                                        <CProgress className="progress-xs" color="danger" value="78" />
+                                    </div>
+                                </div>
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            February
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="56" />
+                                        <CProgress className="progress-xs" color="danger" value="94" />
+                                    </div>
+                                </div>
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            March
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="12" />
+                                        <CProgress className="progress-xs" color="danger" value="67" />
+                                    </div>
+                                </div>
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            April
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="43" />
+                                        <CProgress className="progress-xs" color="danger" value="91" />
+                                    </div>
+                                </div>
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            May
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="22" />
+                                        <CProgress className="progress-xs" color="danger" value="73" />
+                                    </div>
+                                </div>
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            June
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="53" />
+                                        <CProgress className="progress-xs" color="danger" value="82" />
+                                    </div>
+                                </div>
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            July
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="9" />
+                                        <CProgress className="progress-xs" color="danger" value="69" />
+                                    </div>
+                                </div>
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            August
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="9" />
+                                        <CProgress className="progress-xs" color="danger" value="69" />
+                                    </div>
+                                </div>
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            September
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="22" />
+                                        <CProgress className="progress-xs" color="danger" value="73" />
+                                    </div>
+                                </div>
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            October
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="22" />
+                                        <CProgress className="progress-xs" color="danger" value="73" />
+                                    </div>
+                                </div>
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            November
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="22" />
+                                        <CProgress className="progress-xs" color="danger" value="73" />
+                                    </div>
+                                </div>
+                                <div className="progress-group mb-4">
+                                    <div className="progress-group-prepend">
+                                        <span className="progress-group-text">
+                                            December
+                      </span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="info" value="22" />
+                                        <CProgress className="progress-xs" color="danger" value="73" />
+                                    </div>
+                                </div>
+                                <div className="legend text-center">
+                                    <small>
+                                        <sup className="px-1"><CBadge shape="pill" color="info">&nbsp;</CBadge></sup>
+                      New clients
+                      &nbsp;
+                      <sup className="px-1"><CBadge shape="pill" color="danger">&nbsp;</CBadge></sup>
+                      Recurring clients
+                    </small>
+                                </div>
+                            </CCol>
+
+                            <CCol xs="12" md="6" xl="6">
+
+                                <CRow>
+                                    <CCol sm="6">
+                                        <CCallout color="primary">
+                                            <small className="text-muted">Sites Visited</small>
+                                            <br />
+                                            <strong className="h4">78,623</strong>
+                                        </CCallout>
+                                    </CCol>
+                                    <CCol sm="6">
+                                        <CCallout color="success">
+                                            <small className="text-muted">Images analysed</small>
+                                            <br />
+                                            <strong className="h4">49,123</strong>
+                                        </CCallout>
+                                    </CCol>
+                                </CRow>
+
+                                <hr className="mt-0" />
+                                <div className="progress-group">
+                                    <div className="progress-group-header">
+                                        <CIcon className="progress-group-icon" name="cib-instagram" />
+                                        <span className="title">Instagram</span>
+                                        <span className="ml-auto font-weight-bold">191,235 <span className="text-muted small">(56%)</span></span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="success" value="56" />
+                                    </div>
+                                </div>
+
+
+                                <div className="progress-group">
+                                    <div className="progress-group-header">
+                                        <CIcon name="cib-facebook" className="progress-group-icon" />
+                                        <span className="title">Facebook</span>
+                                        <span className="ml-auto font-weight-bold">51,223 <span className="text-muted small">(15%)</span></span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="success" value="15" />
+                                    </div>
+                                </div>
+                                <div className="progress-group">
+                                    <div className="progress-group-header">
+                                        <CIcon name="cib-twitter" className="progress-group-icon" />
+                                        <span className="title">Twitter</span>
+                                        <span className="ml-auto font-weight-bold">37,564 <span className="text-muted small">(11%)</span></span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="success" value="11" />
+                                    </div>
+                                </div>
+                                <div className="progress-group">
+                                    <div className="progress-group-header">
+                                        <CIcon name="cib-linkedin" className="progress-group-icon" />
+                                        <span className="title">Chictopia.com</span>
+                                        <span className="ml-auto font-weight-bold">27,319 <span className="text-muted small">(8%)</span></span>
+                                    </div>
+                                    <div className="progress-group-bars">
+                                        <CProgress className="progress-xs" color="success" value="8" />
+                                    </div>
+                                </div>
+                                <div className="divider text-center">
+                                    <CButton color="link" size="sm" className="text-muted">
+                                        <CIcon name="cil-options" />
+                                    </CButton>
+                                </div>
+
+                            </CCol>
+                        </CRow>
+                    </CModalBody>
+                </CModal>
             </>
         )
     }
